40183ba64cae1f74ca90b5784405a2478278f8aa
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-17 19:22:32 -0300

    Simplified the code

--- JavaCollections/src/br/dev/rodrigocury/utils/StringUtils.java
+++ JavaCollections/src/br/dev/rodrigocury/utils/StringUtils.java
@@ -3,10 +3,10 @@ package br.dev.rodrigocury.utils;
 public class StringUtils {
     public static String capitalize(String string){
         String toReturn = "";
-        String[] strings = string.trim().replaceAll("( )+", " ").split(" ");
+        String[] strings = string.trim().split("( )+");
         for (int i = 0; i < strings.length ; i++) {
             String space = i == 0 ? "" : " ";
-            toReturn = toReturn + space + strings[i].substring(0,1).toUpperCase() + strings[i].substring(1).toLowerCase();
+            toReturn = toReturn.concat(space + strings[i].substring(0,1).toUpperCase() + strings[i].substring(1).toLowerCase());
         }
         return toReturn;
     }
