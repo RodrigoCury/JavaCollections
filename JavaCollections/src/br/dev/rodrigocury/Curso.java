13e43db43e249620f82892d8bb8b21ca70d90075
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-17 19:24:04 -0300

    Adicionado Métodos e tempoTotal, toString com concatenação

--- JavaCollections/src/br/dev/rodrigocury/Curso.java
+++ JavaCollections/src/br/dev/rodrigocury/Curso.java
@@ -2,30 +2,31 @@ package br.dev.rodrigocury;
 
 import br.dev.rodrigocury.utils.StringUtils;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
+    private int tempoTotal = 0;
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new LinkedList<>();
 
-    public Curso (String nome, String instrutor){
+    public Curso(String nome, String instrutor) {
         this.nome = StringUtils.capitalize(nome);
         this.instrutor = StringUtils.capitalize(instrutor);
     }
 
-    public Curso(String nome, String instrutor, List<Aula> aulas){
+    public Curso(String nome, String instrutor, List<Aula> aulas) {
         this(nome, instrutor);
-        for (int i = 0; i<aulas.size(); i++)
-            aulas.get(i).setNumeroDaAula(i+1);
-        this.aulas = aulas;
+        for (int i = 0; i < aulas.size(); i++) {
+            aulas.get(i).setNumeroDaAula(i + 1);
+            this.add(aulas.get(i));
+        }
     }
 
-    public Curso(String nome, String instrutor, Aula ...aula){
-        this(nome, instrutor, new LinkedList<>(List.of(aula)));
+    public Curso(String nome, String instrutor, Aula... aula) {
+        this(nome, instrutor, List.of(aula));
     }
 
     public String getNome() {
@@ -40,25 +41,30 @@ public class Curso {
         return Collections.unmodifiableList(this.aulas);
     }
 
-    public void add(Aula aula){
+    public void add(Aula aula) {
         aula.setNumeroDaAula(this.aulas.size() + 1);
         this.aulas.add(aula);
+        this.tempoTotal += aula.getTempo();
     }
 
-    public void add(Aula ...aulas){
+    public void add(Aula... aulas) {
         List.of(aulas).forEach(this::add);
     }
 
-    public int getTotalAulas(){
+    public int getTotalAulas() {
         return this.aulas.size();
     }
 
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         String toPrint = String.format("Curso: %s - instrutor: %s \nAulas:\n", this.nome, this.instrutor);
         for (Aula aula :
                 this.aulas) {
-            toPrint = toPrint + aula.toString() + "\n";
+            toPrint = toPrint.concat(aula.toString() + "\n");
         }
         return toPrint;
     }
