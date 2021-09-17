f2e553a11a399621a33d6b7e2a5b86e90b07eec3
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-17 18:55:20 -0300

    Classe Aula

---
+++ JavaCollections/src/br/dev/rodrigocury/Aula.java
@@ -0,0 +1,44 @@
+package br.dev.rodrigocury;
+
+import br.dev.rodrigocury.utils.StringUtils;
+
+public class Aula implements Comparable<Aula>{
+    private String titulo;
+    private int tempo;
+    private int numeroDaAula;
+
+    public Aula(String titulo, int tempo){
+        this.titulo = StringUtils.capitalize(titulo);
+        this.tempo = tempo;
+    }
+    public Aula(String titulo, int tempo, int numeroDaAula){
+        this(titulo, tempo);
+        this.numeroDaAula = numeroDaAula;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getNumeroDaAula() {
+        return numeroDaAula;
+    }
+
+    public void setNumeroDaAula(int numeroDaAula) {
+        this.numeroDaAula = numeroDaAula;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Aula: %s - duração: %dmin; #%d", this.titulo, this.tempo, this.numeroDaAula);
+    }
+
+    @Override
+    public int compareTo(Aula outraAula) {
+        return Integer.compare(this.numeroDaAula, outraAula.numeroDaAula);
+    }
+}
